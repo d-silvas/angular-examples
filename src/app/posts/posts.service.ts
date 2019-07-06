2df7820bef3f04359d7df864833d7b3f72b19ff0
David Silva <d.silvas@outlook.com>
2019-07-06 12:19:03 +0100

    Updating posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -45,6 +45,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return {...this.posts.find(p => p.id === id)};
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title, content };
     this.http.post<{ message: string, postId: string }>(this.postsBaseUrl, post)
@@ -53,6 +57,14 @@ export class PostsService {
         post.id = newPostId;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {id, title, content};
+    this.http.put(`${this.postsBaseUrl}/${id}`, post)
+      .subscribe(response => {
+        console.log(response);
       });
   }
 
