dd9761b84f2534a1ec9935861574afaadcc1539a
David Silva <d.silvas@outlook.com>
2019-07-06 11:07:34 +0100

    Update UI after deleting post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -58,7 +58,9 @@ export class PostsService {
   deletePost(postId: string) {
     this.http.delete(`${this.postsBaseUrl}/${postId}`)
       .subscribe(() => {
-        console.log('Deleted!');
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
   }
 }
